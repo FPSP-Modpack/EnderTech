540c510d1a4f00bcd146467a8f64cc11fc6af0a7
glowredman <fredcraft00@gmail.com>
2021-02-06 19:16:48 +0100

    Remove "FPSP Version" from mod name

--- src/main/java/io/endertech/reference/Reference.java
+++ src/main/java/io/endertech/reference/Reference.java
@@ -4,7 +4,7 @@ public class Reference
 {
     public static final String MOD_ID = "EnderTech";
     public static final String CHANNEL_NAME = MOD_ID;
-    public static final String MOD_NAME = "EnderTech FPSP Edition";
+    public static final String MOD_NAME = "EnderTech";
     public static final String VERSION_NUMBER = "@VERSION@";
     public static final int ONE_SECOND_IN_TICKS = 20;
     public static final int NETWORK_UPDATE_RANGE = 192; // CoFH default
